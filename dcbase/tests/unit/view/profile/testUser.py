d8461f6e30d26afd0ef07fba671e4deb369047df
Tim Court <tctimmeh@gmail.com>
2014-11-30 15:10:07 -0700

    added more tests for user profile page

--- dcbase/tests/unit/view/profile/testUser.py
+++ dcbase/tests/unit/view/profile/testUser.py
@@ -6,9 +6,20 @@ class TestUserView(UnitTestCase):
     def setUp(self):
         super().setUp()
         self.user = self.createUser()
-        self.url = reverse('account_profile_user', kwargs={'username': self.user.username})
+        self.url = self._createUrl(self.user.username)
+        self.response = self.client.get(self.url)
+
+    def _createUrl(self, username):
+        return reverse('account_profile_user', kwargs={'username': username})
 
     def test_rendersProfileTemplate(self):
-        response = self.client.get(self.url)
-        self.assertTemplateUsed(response, 'dcbase/profile.html')
+        self.assertTemplateUsed(self.response, 'dcbase/profile.html')
+
+    def test_contextContainsUserAsGivenInUrl(self):
+        self.assertEqual(self.user, self.response.context['profileUser'])
+
+    def test_returnsNotFoundStatusIfUserNameIsInvalid(self):
+        url = self._createUrl(self.randStr())
+        response = self.client.get(url)
+        self.assertResponseStatusIsNotFound(response)
 
