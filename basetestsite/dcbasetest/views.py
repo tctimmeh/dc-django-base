c499e168a60fb8ccbbfae6baf22fd132446d4a8b
Tim Court <tctimmeh@gmail.com>
2015-02-24 20:31:57 -0700

    reworked form templates to allow easier inclusion of single form fields or widgets

--- basetestsite/dcbasetest/views.py
+++ basetestsite/dcbasetest/views.py
@@ -1,6 +1,14 @@
+from django.forms import Form, CharField, BooleanField
 from django.shortcuts import render
 
+class TestForm(Form):
+    text = CharField(max_length=20, help_text="Help!", required=False)
+    errors = CharField(max_length=10, required=False)
+    option = BooleanField(help_text="It's a good option...", required=False)
 
 def home(request):
-    return render(request, 'dcbasetest/home.html')
+    form = TestForm(data={'text': '', 'errors': '', 'option': False})
+    form.add_error(None, "Form-level error")
+    form.add_error('errors', 'Field error')
+    return render(request, 'dcbasetest/home.html', {'form': form})
 
